82cf56949d1771162e5dcd74446066e806c552d2
Serhiy Savchuk and Wei Kuang <wei@>
2019-06-10 10:35:27 -0700

    add code to index.html, styles.css, and scripts.js.

--- js/scripts.js
+++ js/scripts.js
@@ -1,100 +1,22 @@
-
 $(document).ready(function(){
-  $("#form-one").submit(function(){
 
-    // Define variables
-    var waffles =
-    $("#waffles").val();
+ $(".fav").submit(function(event){
 
-    var experience =
-    $("#experience").val();
+   var text1 =
+   $ (".form-control1").val();
+   var text2 =
+   $(".form-control2").val();
+   var text3 =
+   $(".form-control3").val();
 
-    var platform =
-    $("#platform").val();
+   var textCombined = [];
+   textCombined.push(text1,text2,text3);
 
-    var os =
-    $("#os").val();
+   $("#list1").show();
+   $("#1").append(textCombined[0]);
+   $("#2").append(textCombined[1]);
+   $("#3").append(textCombined[2]);
 
-    var frontbackend =
-    $("#frontbackend").val();
-
-    // To show a recommendation based on user's selections and hide previous recommendation(s).
-    if (waffles === "no") {
-      $(".language").hide();
-      $("#disqualified").show();
-
-    } else if (waffles === "yes" && experience === "one" && platform === "desktop" && os === "macos" && frontbackend === "frontend") {
-      $(".language").hide();
-      $("#python").show();
-
-    } else if (waffles === "yes" && experience === "one" && platform === "desktop" && os === "macos" && frontbackend === "backend") {
-      $(".language").hide();
-      $("#javascript").show();
-
-    } else if (waffles === "yes" && experience === "one" && platform === "desktop" && os === "windows" && frontbackend === "frontend") {
-      $(".language").hide();
-      $("#python").show();
-
-    } else if (waffles === "yes" && experience === "one" && platform === "desktop" && os === "windows" && frontbackend === "backend") {
-      $(".language").hide();
-      $("#javascript").show();
-
-    } else if (waffles === "yes" && experience === "one" && platform === "web" && os === "macos" && frontbackend === "frontend") {
-      $(".language").hide();
-      $("#python").show();
-
-    } else if (waffles === "yes" && experience === "one" && platform === "web" && os === "macos" && frontbackend === "backend") {
-      $(".language").hide();
-      $("#python").show();
-
-    } else if (waffles === "yes" && experience === "one" && platform === "web" && os === "windows" && frontbackend === "frontend") {
-      $(".language").hide();
-      $("#python").show();
-
-    } else if (waffles === "yes" && experience === "one" && platform === "web" && os === "windows" && frontbackend === "backend") {
-      $(".language").hide();
-      $("#javascript").show();
-
-    } else if (waffles === "yes" && experience === "two" && platform === "desktop" && os === "macos" && frontbackend === "frontend") {
-      $(".language").hide();
-      $("#swift").show();
-
-    } else if (waffles === "yes" && experience === "two" && platform === "desktop" && os === "macos" && frontbackend === "backend") {
-      $(".language").hide();
-      $("#swift").show();
-
-    } else if (waffles === "yes" && experience === "two" && platform === "desktop" && os === "windows" && frontbackend === "frontend") {
-      $(".language").hide();
-      $("#c").show();
-
-    } else if (waffles === "yes" && experience === "two" && platform === "desktop" && os === "windows" && frontbackend === "backend") {
-      $(".language").hide();
-      $("#c").show();
-
-    } else if (waffles === "yes" && experience === "two" && platform === "web" && os === "macos" && frontbackend === "frontend") {
-      $(".language").hide();
-      $("#swift").show();
-
-    } else if (waffles === "yes" && experience === "two" && platform === "web" && os === "macos" && frontbackend === "backend") {
-      $(".language").hide();
-      $("#swift").show();
-
-    } else if (waffles === "yes" && experience === "two" && platform === "web" && os === "windows" && frontbackend === "frontend") {
-      $(".language").hide();
-      $("#c").show();
-
-    } else if (waffles === "yes" && experience === "two" && platform === "web" && os === "windows" && frontbackend === "backend") {
-      $(".language").hide();
-      $("#c").show();
-    }
-
-    // To block default click handling
     event.preventDefault();
-
-    // To enable tooltips
-    $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
-    })
-
-  });
+ });
 });
